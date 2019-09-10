dee5eba3674b6a11b945349984a38493ff77eba4
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-10 20:13:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+  
+  def intialize(l1, l2, l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+  end
+  
+  def valid?
+      (l1+l2 <= l3) || (l1+l3 <= l2) || (l2+l3 <= l1) && l1<=0 ||l2 <=0||l3 <=0
+      raise TriangleError
+  end
+  
+  def kind 
+    valid?
+    if l1 == l2 && l2 == l3 
+        :equilateral
+    elsif l1 == l2 || l2 == l3 || l1==l3
+        :isosceles
+    else
+        :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
