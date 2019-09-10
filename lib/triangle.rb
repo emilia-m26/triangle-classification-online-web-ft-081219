dd9827c4e2b2d7af7e6f23581717a3c5937bfe42
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-10 20:24:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,13 @@ class Triangle
     @l3=l3
   end
   
-  def valid?
-      (l1+l2 <= l3) || (l1+l3 <= l2) || (l2+l3 <= l1) && l1<=0 ||l2 <=0||l3 <=0
-      raise TriangleError
-  end
+  # def valid?
+  #     (l1+l2 <= l3) || (l1+l3 <= l2) || (l2+l3 <= l1) && l1<=0 ||l2 <=0||l3 <=0
+  #     raise TriangleError
+  # end
   
   def kind 
-    valid?
+    # valid?
     if l1 == l2 && l2 == l3 
         :equilateral
     elsif l1 == l2 || l2 == l3 || l1==l3
